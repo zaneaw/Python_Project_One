782fff79998b62736928ebcd7ac69b566ee4e2fa
zaneaw <wilsonzanea@gmail.com>
2020-10-27 18:11:47 -0400

    Before handling errors and exceptions

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,9 @@
 import random
 
 welcome_signs = "=" * 43
-print(
-    "{}\n    WELCOME to Zane's Zany Number Game!   \n{}".format(
-        welcome_signs, welcome_signs
-    )
-)
+print("{}\n    WELCOME to Zane's Zany Number Game!   \n{}".format(welcome_signs, welcome_signs))
 name = input("\nWhat is your name, contestant? ").title()
+hiscore_list = []
 
 
 def start_game():
@@ -14,10 +11,11 @@ def start_game():
         answer = input("{}, would you like to play again? (Yes / No) ".format(name))
         answer.lower()
         if answer == "yes" or answer == "y":
+            print("\nGreat! Try to beat your hiscore of {}!".format(min(hiscore_list)))
             start_game()
             answer
         else:
-            print("\nThanks for playing!")
+            print("\nYour hiscore was {}! See you next time!".format(min(hiscore_list)))
 
     guess_attempts = 1
     random_number = random.randint(1, 10)
@@ -25,30 +23,26 @@ def start_game():
     print(random_number)
 
     print("\nGood luck, {}!".format(name))
-    guess = int(
-        input("\nI'm thinking of a number between 1 and 10. Can you guess it? ")
-    )
-    while True:
+    guess = int(input("\nI'm thinking of a number between 1 and 10. Can you guess it? "))
 
+    while True:
         if guess != random_number:
             if guess < 1 or guess > 10:
-                guess = int(input("TRY AGAIN!\nThe number is between 1 and 10! "))
+                guess = int(input("\nTRY AGAIN!\nThe number is between 1 and 10! "))
             elif guess > random_number:
-                guess = int(input("WRONG!\nThe number is going to be lower! "))
+                guess = int(input("\nWRONG!\nThe number is going to be lower! "))
                 guess_attempts += 1
             elif guess < random_number:
-                guess = int(input("WRONG!\nThe number is going to be higher! "))
+                guess = int(input("\nWRONG!\nThe number is going to be higher! "))
                 guess_attempts += 1
         elif guess_attempts > 1:
-            print(
-                "CORRECT!\nIt only took you {} attempts to guess the right number!".format(
-                    guess_attempts
-                )
-            )
+            print("CORRECT!\nIt only took you {} attempts to guess the right number!".format(guess_attempts))
+            hiscore_list.append(guess_attempts)
             play_again()
             break
         else:
-            print("Nice job, you guessed it on the first try!")
+            print("\nNice job, you guessed it on the first try!")
+            hiscore_list.append(guess_attempts)
             play_again()
             break
 
